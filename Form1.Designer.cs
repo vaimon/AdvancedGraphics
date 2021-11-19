b468ded0ac6b45f047abf5707f3285d401c4fe57
Andrew Vaimon <niko142000@mail.ru>
2021-11-19 10:59:48 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace AdvancedGraphics
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEditor = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.btnShowAxis = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textShiftZ = new System.Windows.Forms.TextBox();
+            this.textShiftY = new System.Windows.Forms.TextBox();
+            this.textShiftX = new System.Windows.Forms.TextBox();
+            this.buttonShift = new System.Windows.Forms.Button();
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.rbPerspective = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.z_buffer = new System.Windows.Forms.Button();
+            this.checkBoxPruneNonFacial = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEditor
+            // 
+            this.btnEditor.Location = new System.Drawing.Point(49, 12);
+            this.btnEditor.Name = "btnEditor";
+            this.btnEditor.Size = new System.Drawing.Size(233, 55);
+            this.btnEditor.TabIndex = 0;
+            this.btnEditor.TabStop = false;
+            this.btnEditor.Text = "Редактор фигур";
+            this.btnEditor.UseVisualStyleBackColor = true;
+            this.btnEditor.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 25;
+            this.listBox.Location = new System.Drawing.Point(21, 30);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(271, 254);
+            this.listBox.TabIndex = 1;
+            this.listBox.TabStop = false;
+            this.listBox.SelectedIndexChanged += new System.EventHandler(this.listBox_SelectedIndexChanged);
+            // 
+            // btnShowAxis
+            // 
+            this.btnShowAxis.Location = new System.Drawing.Point(46, 609);
+            this.btnShowAxis.Margin = new System.Windows.Forms.Padding(2);
+            this.btnShowAxis.Name = "btnShowAxis";
+            this.btnShowAxis.Size = new System.Drawing.Size(233, 44);
+            this.btnShowAxis.TabIndex = 27;
+            this.btnShowAxis.TabStop = false;
+            this.btnShowAxis.Text = "Показать оси";
+            this.btnShowAxis.UseVisualStyleBackColor = true;
+            this.btnShowAxis.Click += new System.EventHandler(this.btnShowAxis_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(206, 336);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 25);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "Z:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(110, 336);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 25);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Y:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 336);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 25);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Х:";
+            // 
+            // textShiftZ
+            // 
+            this.textShiftZ.Enabled = false;
+            this.textShiftZ.Location = new System.Drawing.Point(242, 333);
+            this.textShiftZ.Margin = new System.Windows.Forms.Padding(2);
+            this.textShiftZ.MaxLength = 5;
+            this.textShiftZ.Name = "textShiftZ";
+            this.textShiftZ.Size = new System.Drawing.Size(50, 31);
+            this.textShiftZ.TabIndex = 21;
+            this.textShiftZ.TabStop = false;
+            this.textShiftZ.Text = "0";
+            // 
+            // textShiftY
+            // 
+            this.textShiftY.Enabled = false;
+            this.textShiftY.Location = new System.Drawing.Point(146, 333);
+            this.textShiftY.Margin = new System.Windows.Forms.Padding(2);
+            this.textShiftY.MaxLength = 5;
+            this.textShiftY.Name = "textShiftY";
+            this.textShiftY.Size = new System.Drawing.Size(50, 31);
+            this.textShiftY.TabIndex = 22;
+            this.textShiftY.TabStop = false;
+            this.textShiftY.Text = "0";
+            // 
+            // textShiftX
+            // 
+            this.textShiftX.Enabled = false;
+            this.textShiftX.Location = new System.Drawing.Point(50, 333);
+            this.textShiftX.Margin = new System.Windows.Forms.Padding(2);
+            this.textShiftX.MaxLength = 5;
+            this.textShiftX.Name = "textShiftX";
+            this.textShiftX.Size = new System.Drawing.Size(50, 31);
+            this.textShiftX.TabIndex = 23;
+            this.textShiftX.TabStop = false;
+            this.textShiftX.Text = "0";
+            // 
+            // buttonShift
+            // 
+            this.buttonShift.Enabled = false;
+            this.buttonShift.Location = new System.Drawing.Point(57, 368);
+            this.buttonShift.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonShift.Name = "buttonShift";
+            this.buttonShift.Size = new System.Drawing.Size(201, 44);
+            this.buttonShift.TabIndex = 20;
+            this.buttonShift.TabStop = false;
+            this.buttonShift.Text = "Сместить";
+            this.buttonShift.UseVisualStyleBackColor = true;
+            this.buttonShift.Click += new System.EventHandler(this.buttonShift_Click);
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.canvas.Dock = System.Windows.Forms.DockStyle.Right;
+            this.canvas.Location = new System.Drawing.Point(334, 0);
+            this.canvas.Margin = new System.Windows.Forms.Padding(2);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(1344, 944);
+            this.canvas.TabIndex = 19;
+            this.canvas.TabStop = false;
+            this.canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseDown);
+            this.canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseMove);
+            this.canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseUp);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnDelete);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.listBox);
+            this.groupBox1.Controls.Add(this.buttonShift);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textShiftX);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textShiftY);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textShiftZ);
+            this.groupBox1.Location = new System.Drawing.Point(8, 76);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(317, 428);
+            this.groupBox1.TabIndex = 28;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Сцена";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackgroundImage = global::AdvancedGraphics.Properties.Resources.delete;
+            this.btnDelete.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnDelete.Enabled = false;
+            this.btnDelete.Location = new System.Drawing.Point(114, 290);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(50, 38);
+            this.btnDelete.TabIndex = 28;
+            this.btnDelete.TabStop = false;
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(170, 290);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(122, 38);
+            this.btnAdd.TabIndex = 27;
+            this.btnAdd.TabStop = false;
+            this.btnAdd.Text = "Загрузить...";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Текстовые файлы|*.txt";
+            this.openFileDialog1.InitialDirectory = "C:\\Code\\3DFacesProcessing\\shapes";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::AdvancedGraphics.Properties.Resources._cc3333_0008e6_01bc0d_1920_1080;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(43, 606);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(239, 50);
+            this.pictureBox1.TabIndex = 29;
+            this.pictureBox1.TabStop = false;
+            // 
+            // rbPerspective
+            // 
+            this.rbPerspective.AutoSize = true;
+            this.rbPerspective.BackColor = System.Drawing.SystemColors.Control;
+            this.rbPerspective.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.rbPerspective.Checked = true;
+            this.rbPerspective.Location = new System.Drawing.Point(45, 536);
+            this.rbPerspective.Margin = new System.Windows.Forms.Padding(2);
+            this.rbPerspective.Name = "rbPerspective";
+            this.rbPerspective.Size = new System.Drawing.Size(246, 29);
+            this.rbPerspective.TabIndex = 30;
+            this.rbPerspective.TabStop = true;
+            this.rbPerspective.Text = "Перспективная проекция";
+            this.rbPerspective.UseVisualStyleBackColor = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.BackColor = System.Drawing.SystemColors.Control;
+            this.radioButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.radioButton1.Location = new System.Drawing.Point(45, 569);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(190, 29);
+            this.radioButton1.TabIndex = 30;
+            this.radioButton1.Text = "Взгляд со стороны";
+            this.radioButton1.UseVisualStyleBackColor = false;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.rbParallel_CheckedChanged);
+            // 
+            // z_buffer
+            // 
+            this.z_buffer.Location = new System.Drawing.Point(43, 701);
+            this.z_buffer.Name = "z_buffer";
+            this.z_buffer.Size = new System.Drawing.Size(236, 49);
+            this.z_buffer.TabIndex = 31;
+            this.z_buffer.Text = "Удалить невидимые";
+            this.z_buffer.UseVisualStyleBackColor = true;
+            this.z_buffer.Click += new System.EventHandler(this.z_buffer_Click);
+            // 
+            // checkBoxPruneNonFacial
+            // 
+            this.checkBoxPruneNonFacial.AutoSize = true;
+            this.checkBoxPruneNonFacial.Location = new System.Drawing.Point(65, 756);
+            this.checkBoxPruneNonFacial.Name = "checkBoxPruneNonFacial";
+            this.checkBoxPruneNonFacial.Size = new System.Drawing.Size(190, 29);
+            this.checkBoxPruneNonFacial.TabIndex = 32;
+            this.checkBoxPruneNonFacial.Text = "Отсечь нелицевые";
+            this.checkBoxPruneNonFacial.UseVisualStyleBackColor = true;
+            this.checkBoxPruneNonFacial.CheckedChanged += new System.EventHandler(this.checkBoxPruneNonFacial_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1678, 944);
+            this.Controls.Add(this.checkBoxPruneNonFacial);
+            this.Controls.Add(this.z_buffer);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.rbPerspective);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnShowAxis);
+            this.Controls.Add(this.canvas);
+            this.Controls.Add(this.btnEditor);
+            this.Controls.Add(this.pictureBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Просмотр сцены";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEditor;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button btnShowAxis;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textShiftZ;
+        private System.Windows.Forms.TextBox textShiftY;
+        private System.Windows.Forms.TextBox textShiftX;
+        private System.Windows.Forms.Button buttonShift;
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton rbPerspective;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button z_buffer;
+        private System.Windows.Forms.CheckBox checkBoxPruneNonFacial;
+    }
+}
